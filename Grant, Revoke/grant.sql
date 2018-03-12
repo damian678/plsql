a45d6b575f96c2e3baf2a000d3f5db50dab40540
Damian <34041127+damian678@users.noreply.github.com>
2018-03-12 14:25:36 +0100

    Update grant.sql

--- Grant, Revoke/grant.sql
+++ Grant, Revoke/grant.sql
@@ -2,27 +2,29 @@ create table czytelnicy(id_czytelnika number primary key, imie varchar2(50), naz
 create table ksiazki(sygnatura number primary key, autor varchar2(50), 
 tytul varchar2(500), id_czytelnika number references czytelnicy);
 
-insert into czytelnicy  values (1, 'twoje-imiê', 'twoje-nazwisko');
-insert into ksiazki values (1, 'zaproponuj-autora', 'zaproponuj-tytu³', 1);
+insert into czytelnicy  values (1, 'twoje-imiÃª', 'twoje-nazwisko');
+insert into ksiazki values (1, 'zaproponuj-autora', 'zaproponuj-tytul', 1);
 
-grant select on czytelnicy to mat17uzfina4;--nadaje uprawnienia u¿ytkownikowi
+grant select on czytelnicy to mat17uzfina4;--nadaje uprawnienia uÅ¼ytkownikowi dla select
 
 select * from mat17uzfina4.czytelnicy;
 insert into  mat17uzfina4.czytelnicy values(2, 'Jan', 'Kowalski');
 
 select * from user_tab_privs;
 
-revoke select on czytelnicy from mat17uzfina4;--odbiera uprawnienia u¿ytkownikowi
+revoke select on czytelnicy from mat17uzfina4;--odbiera uprawnienia uÅ¼ytkownikowi dla select
 select * from mat17uzfina4;
+
 --6
-grant select, insert on ksiazki to mat17uzfina4;-- nadaje uprawnienia u¿ytkownikow z tabeli ksi¹¿ki 
---do wstawiania wierszy do tej tabeli
+grant select, insert on ksiazki to mat17uzfina4;-- nadaje uprawnienia uÅ¼ytkownikow z tabeli ksiazki 
+--do wstawiania wierszy i dla select do tej tabeli
 select * from mat17uzfina4.ksiazki;
 insert into  mat17uzfina4.ksiazki values(2, 'Celko', 'SQL', null);
 select * from mat17uzfina4.ksiazki;
 SELECT * FROM ksiazki;
-revoke select,insert on ksiazki from  mat17uzfina4;--odbiera uprawnienia u¿ytkownikow z tabeli ksi¹¿ki 
---do wstawiania wierszy do tej tabeli
+revoke select,insert on ksiazki from  mat17uzfina4;--odbiera uprawnienia uÂ¿ytkownikow z tabeli ksiazki 
+--do wstawiania wierszy i dla select do tej tabeli
+
 --8
 grant select,insert,update,delete on czytelnicy to mat17uzfina4;
 select * from mat17uzfina4.czytelnicy;
@@ -30,15 +32,17 @@ select * from mat17uzfina4.czytelnicy;
 insert into mat17uzfina4.czytelnicy values (2,'Jan','Kowalski');
 update mat17uzfina4.czytelnicy set imie='Adam' where id_czytelnika=2;
 delete from mat17uzfina4.czytelnicy where id_czytelnika=2;
+
 --10
 create table ksiazki2(sygnatura number primary key, autor varchar2(50), tytul varchar2(500), 
 id_czytelnika references 
-mat17uzfina4.czytelnicy);--nie posiada uprawnieñ dla kluczy obcych
+mat17uzfina4.czytelnicy);--nie posiada uprawnieÅ„ dla nadawania kluczy obcych
 
 grant references on czytelnicy to mat17uzfina4;
 create table ksiazki2(sygnatura number primary key, autor varchar2(50), tytul varchar2(500), 
 id_czytelnika references 
-mat17uzfina4.czytelnicy);--teraz siê udalo
+mat17uzfina4.czytelnicy);--teraz siÄ™ udaÅ‚o
+
 --12
 grant update (autor,tytul),insert(sygnatura,autor,tytul),select on ksiazki to mat17uzfina4;--uprawnienie do modyfikacji kolumn
 
@@ -50,11 +54,13 @@ select * from mat17uzfina4.ksiazki;
 update mat17uzfina4.ksiazki set id_czytelnika=null where sygnatura=1;--niewystarczajace uprawnienia
 
 update mat17uzfina4.ksiazki set tytul=upper(tytul) where sygnatura=1;
+
 --13
 grant alter on ksiazki to mat17uzfina4;--nadaje uprawnienia do modyfikacji tabel ksiazki
 alter table mat17uzfina4.ksiazki add(rok number(4));
 select * from mat17uzfina4.ksiazki;
 select * from ksiazki;
+
 --14
 grant select, insert, delete on ksiazki to mat17uzfina4 with grant option;
 --grant select,insert, delete on mat17uzfina4.ksiazki to mat17uzfina? with grant option;
@@ -78,6 +84,7 @@ grant select on sequence_id_studenta to public;
 insert into lista_studentow values(sequence_id_studenta.nextval,'Damian','Bilski','m');
 select distinct imie,nazwisko  from lista_studentow order by nazwisko;
 commit;
+
 --19 
 create view studentki as
 select * from lista_studentow where plec='k';
